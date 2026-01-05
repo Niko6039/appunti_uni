e2721f8a67ac1a2d9b8d188e8094fa383b8545ed
niko6039 <tauronicolo762@gmail.com>
2026-01-05 19:47:29 +0100

    typedef struct

---
+++ typedef/stuct_studenti.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    char nome_cognome[20];
+    int numero_tessera;
+    int matricola;
+} studenti;
+
+int main()
+{
+    studenti s1;
+    char nome[20];
+    // primo studente
+    printf("Nome e Cognome(char): ");
+    scanf(" %s", &s1.nome_cognome);
+    // strcpy(s1.nome_cognome, nome);
+
+    printf("Numero Tessera(int): ");
+    scanf("%d", &s1.numero_tessera);
+
+    printf("Matricila(int): ");
+    scanf("%d", &s1.matricola);
+
+    printf("Studente: %s\n", s1.nome_cognome);
+    printf("Numero Tessera: %d\n", s1.numero_tessera);
+    printf("Matricila: %d\n", s1.matricola);
+
+    return 0;
+}
