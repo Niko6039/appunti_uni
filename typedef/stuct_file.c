0549b1856d52910d2f727145ea4a367d153d541c
niko6039 <tauronicolo762@gmail.com>
2026-01-07 15:35:55 +0100

    AGG

--- typedef/stuct_file.c
+++ typedef/stuct_file.c
@@ -3,29 +3,38 @@
 #include <time.h>
 #include <string.h>
 #define MAX 30
+
 typedef struct
 {
     char cod[10];
     int n;
     float mis;
 } SENSORE;
+
 int main()
 {
     srand(time(NULL));
     int n_random = MAX;
     FILE *fp;
+    //
     fp = fopen("sensori.txt", "wb");
     if (fp == NULL)
     {
         perror("Errore nell'apertura del file (scrittura)!\n");
         return 1;
     }
+
     SENSORE sens[MAX];
     for (int i = 0; i < n_random; i++)
     {
+        //          array         int   i + 1
         sprintf(sens[i].cod, "%d", i + 1);
         sens[i].n = rand() % 31;
         sens[i].mis = (float)rand() / (float)(RAND_MAX / MAX);
+        //     indirizzo di sens[i],
+        //               dimensione del sens
+        //
+        //
         fwrite(&sens[i], sizeof(SENSORE), 1, fp);
     }
     fclose(fp);
