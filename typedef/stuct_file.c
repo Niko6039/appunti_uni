b961d62a7575b8d7e47c1b2d080f221920768808
niko6039 <tauronicolo762@gmail.com>
2026-01-07 15:11:29 +0100

    file prof

---
+++ typedef/stuct_file.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#define MAX 30
+typedef struct
+{
+    char cod[10];
+    int n;
+    float mis;
+} SENSORE;
+int main()
+{
+    srand(time(NULL));
+    int n_random = MAX;
+    FILE *fp;
+    fp = fopen("sensori.txt", "wb");
+    if (fp == NULL)
+    {
+        perror("Errore nell'apertura del file (scrittura)!\n");
+        return 1;
+    }
+    SENSORE sens[MAX];
+    for (int i = 0; i < n_random; i++)
+    {
+        sprintf(sens[i].cod, "%d", i + 1);
+        sens[i].n = rand() % 31;
+        sens[i].mis = (float)rand() / (float)(RAND_MAX / MAX);
+        fwrite(&sens[i], sizeof(SENSORE), 1, fp);
+    }
+    fclose(fp);
+    printf("Salvataggio Completato!\n");
+    fp = fopen("sensori.txt", "rb");
+    if (fp == NULL)
+    {
+        perror("Errore nell'apertura del file (lettura)!\n");
+        return 1;
+    }
+    SENSORE lettura;
+    while (fread(&lettura, sizeof(SENSORE), 1, fp) == 1)
+    {
+        printf("Codice: %s - Num: %d - Misura: %.2f\n", lettura.cod, lettura.n, lettura.mis);
+    }
+    fclose(fp);
+    return 0;
+}
