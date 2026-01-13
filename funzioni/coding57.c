f099599c4b9e2aea605dc5af5046a443fb02be93
niko6039Port <tauronicolo762@gmail.com>
2026-01-13 18:02:50 +0100

    Implement random number generation and user input handling in C program

--- funzioni/coding57.c
+++ funzioni/coding57.c
@@ -6,7 +6,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define max 99
+
+void causale(int v[])
+{
+    for (int i = 0; i < max; i++)
+    {
+        v[i] = rand() % 100 + 1;
+        printf("%d ", v[i]);
+        if (i % 11 == 10)
+        {
+            printf("\n");
+        }
+    }
+    printf("\n\n");
+}
 
 int main()
 {
+    srand(time(NULL));
+    int array[max];
+    causale(array);
+    int x = 1;
+    while (x != 0)
+    {
+        printf("Scegli un numero (digita 0 per uscire): ");
+        scanf("%d", &x);
+    }
 }
