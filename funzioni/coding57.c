a67e503ba660522c5ec05a975a1a02f4136521f3
niko6039Port <tauronicolo762@gmail.com>
2026-01-14 13:33:12 +0100

    adf

--- funzioni/coding57.c
+++ funzioni/coding57.c
@@ -1,36 +1,68 @@
-// Scrivere un programmino in C che una volta caricato in modo
-// random un vettore di 10 numeri interi chiede all’utente un
-// numero intero a tre cifre. Gli indici corrispondenti alle cifre del
-// numero intero dato in input dall’utente vengono azzerati. Il
-// programma da in output il vettore modificato
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define max 99
+
+#define MAX 99
+
+void stampa(int v[])
+{
+    printf("\n--- Stato attuale dell'array ---\n");
+    for (int i = 0; i < MAX; i++)
+    {
+        if (v[i] == 0)
+            printf("__ "); // Mostra uno spazio vuoto per i numeri cancellati
+        else
+            printf("%02d ", v[i]);
+
+        if (i % 11 == 10)
+            printf("\n");
+    }
+    printf("\n");
+}
 
 void causale(int v[])
 {
-    for (int i = 0; i < max; i++)
+    for (int i = 0; i < MAX; i++)
     {
         v[i] = rand() % 100 + 1;
-        printf("%d ", v[i]);
-        if (i % 11 == 10)
+    }
+}
+
+void cancella(int v[], int x)
+{
+    int trovati = 0;
+    for (int i = 0; i < MAX; i++)
+    {
+        if (v[i] == x)
         {
-            printf("\n");
+            v[i] = 0;
+            trovati++;
         }
     }
-    printf("\n\n");
+    if (trovati > 0)
+        printf("\n[OK] Numero %d rimosso %d volte.\n", x, trovati);
+    else
+        printf("\n[!] Numero %d non trovato.\n", x);
 }
 
 int main()
 {
     srand(time(NULL));
-    int array[max];
+    int array[MAX];
     causale(array);
-    int x = 1;
+
+    int x = -1;
     while (x != 0)
     {
-        printf("Scegli un numero (digita 0 per uscire): ");
+        stampa(array);
+        printf("Scegli un numero da cancellare (0 per uscire): ");
         scanf("%d", &x);
+        if (x != 0)
+        {
+            cancella(array, x);
+        }
     }
+
+    printf("Programma terminato.\n");
+    return 0;
 }
