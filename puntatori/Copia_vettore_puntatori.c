b8da311b4c62d68057accbbc8f982125e2eb0b55
niko6039Port <tauronicolo762@gmail.com>
2026-01-17 17:50:28 +0100

    ciao

--- puntatori/Copia_vettore_puntatori.c
+++ puntatori/Copia_vettore_puntatori.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define MAX 30
+#define DIMARRAY 30
 int main()
 {
     int v1[DIMARRAY];
---
+++ puntatori/es_ale.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 15
+
+int main()
+{
+}
