838a56de32c83c4f1ebf666c230497eb3c73f0ca
niko6039Port <tauronicolo762@gmail.com>
2026-01-27 14:40:14 +0100

    sdfgh

--- stringe/passaggioVettore_a_funzione2.c
+++ stringe/passaggioVettore_a_funzione2.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+int menu()
+{
+    int sc;
+    do
+    {
+        printf("Cosa vuoi : \n1)Pizza \n2)Sushi \n3)Niente\n");
+        scanf("%d", &sc);
+    } while (sc < 0 && sc > 4);
+    return sc;
+}
+
 void main()
 {
-    printf("\a");
+    int scelta = menu();
+    if (scelta == 1)
+    {
+        printf("Sei normale:)");
+    }
+    else if (scelta == 2)
+    {
+        printf("Crizy!!!");
+    }
+    else
+    {
+        printf("Allora alla prossima!!");
+    }
 }
